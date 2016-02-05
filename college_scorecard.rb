fb1836bac6bcee1855c1a36c19f737d59bf6f0f0
acareaga <aaroncareaga@gmail.com>
2016-02-04 19:45:31 -0700

    Debt working, need to format response with parens

--- college_scorecard.rb
+++ college_scorecard.rb
@@ -2,17 +2,16 @@ require 'csv'
 require 'pry'
 
 class ScoreCard
-
- attr_reader :contents, :max
+  attr_reader :contents, :max
 
   def search_file(attribute)
-    @contents.select do |row|
-      row[:instnm] if (row[:stabbr] == attribute)
-    end
+    @contents.select { |row| row[:instnm] if (row[:stabbr] == attribute) }
   end
 
   def open_file
-    @contents = CSV.open "./2013_college_scorecards.csv", headers: true, header_converters: :symbol
+    @contents = CSV.open "./2013_college_scorecards.csv",
+                    headers: true,
+                    header_converters: :symbol
   end
 
   def get_colleges(state)
@@ -21,42 +20,38 @@ class ScoreCard
 
   def get_top_sal(num)
     salaries_by_college = {}
-    @contents.each do |row|
-      salary = row[:avgfacsal].rjust(10, "0")
-      college = row[:instnm]
-      if salary == "NULL"
-        salary = "0000"
-      end
-    salaries_by_college[salary] = college
-    end
+    @contents.each { |row| salary = row[:avgfacsal].rjust(10, "0")
+                           salary = "0000" if salary == "NULL"
+                          college = row[:instnm]
+      salaries_by_college[salary] = college
+                    }
     return_sorted_sal(num, salaries_by_college).reverse
   end
 
   def return_sorted_sal(num, salaries_by_college)
-    sorted = salaries_by_college.sort_by do |key, value|
-      key
-    end.last(num)
-    universities = sorted.map do |array|
-      array[1]
-    end
+    sorted = salaries_by_college.sort_by { |key, value| key }.last(num)
+    universities = sorted.map { |array| array[1] }
   end
 
   def college_debt
     debt_by_college = {}
-    @contents.each do |row|
-      debt = row[:grad_debt_mdn]
-      college = row[:instnm]
-      debt_by_college[debt] = college
-    end
+    @contents.each { |row| debt = row[:grad_debt_mdn]
+                        college = row[:instnm]
+          debt_by_college[debt] = college
+                    }
     debt_sorter(debt_by_college)
   end
 
   def debt_sorter(debt_by_college)
-    sorted = debt_by_college.find_all do |k,v|
-      k.to_i.between?(1500, 2300)
-    end
+    schools = debt_by_college.sort.to_h.find_all { |k,v| k.between?("1500", "1600") }
+    format_schools_by_debt(schools)
   end
 
+  def format_schools_by_debt(schools)
+    list = schools.each { |debt, school| school }
+                  .reverse
+                  .map { |debt, school| [school, debt] }
+  end
 end
 
 #  max = @contents.max(num) { |row_a, row_b| row_a[:avgfacsal]<=>row_b[:avgfacsal] }
